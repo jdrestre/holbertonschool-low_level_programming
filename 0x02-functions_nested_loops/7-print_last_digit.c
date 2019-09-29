bdc7391d0a66aa294b42cb999dd8ab64629b7d47
jdrestre <1238@holbertonschool.com>
2019-09-29 19:59:28 +0000

    I was printing (044 and needed 8044, I don't know what I changed to get the result they asked me for, is a doubt

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,16 +3,18 @@
  * print_last_digit - print last digit of a number
  * @c: integer
  *
+ *
  * Return: last digit
  */
 int print_last_digit(int c)
 {
 	c = c % 10;
 
-	if (c < 10)
+	if (c < 0)
 	{
-		c = c * (-1);
+		c = c * -1;
 	}
+
 	_putchar('0' + c);
 	return (c);
 }
