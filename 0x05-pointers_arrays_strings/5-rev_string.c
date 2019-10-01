9f0f45d87bb202db45679af8a9a22b3f3af1b0a0
jdrestre <1238@holbertonschool.com>
2019-10-01 23:48:51 +0000

    Trying to understand the use of array and applying it to the task with loops while

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * rev_string - reverses a string.
+ * @s: variable
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int f;	int r;
+	char z[10];
+
+	f = 0;
+	r = 0;
+
+	while (s[f] != '\0')
+	{
+		z[f] = s[f];
+		f++;
+	}
+	f--;
+	while (f >= 0)
+	{
+		s[f] = z[r];
+		f--;
+		r++;
+	}
+	s[r++] = '\0';
+}
