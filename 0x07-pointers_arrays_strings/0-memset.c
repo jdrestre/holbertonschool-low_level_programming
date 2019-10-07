8d74ba205159bd2c08eed3d0a189a3afc6242c44
jdrestre <1238@holbertonschool.com>
2019-10-07 19:08:33 +0000

    Trying to understand what the memset function _memory_configurator_ does when receiving these 3 parameters and what happens in memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @b.
- * @s: A pointer to the memory area to be filled.
- * @b: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
+ * _memset - Entry point
+ * @s: char variable
+ * @b: char variable
+ * @n: int variable
  *
- * Return: A pointer to the filled memory area @s.
+ * Return: s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
