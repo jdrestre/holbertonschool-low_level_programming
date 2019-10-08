fd05a01ea771c855805a295959c19c7a9a449a4e
jdrestre <1238@holbertonschool.com>
2019-10-08 00:05:45 +0000

    trying

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strstr - locate a substring
+ * @haystack: string to be looked through
+ * @needle: substring to be searched for in haystack
+ *
+ * Return: first occurence of the substring needle in haystack,
+ * or NULL is substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
