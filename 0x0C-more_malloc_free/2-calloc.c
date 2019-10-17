d9c778674acae0da29f6f30f6e8a9b4f5dc2b070
jdrestre <1238@holbertonschool.com>
2019-10-17 23:51:52 +0000

    learning to allocates memory for an array, using malloc. If nmemb or size is 0, then _calloc returns NULL, this requeriment was very complicated

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ *
+ * @nmemb: number of elements.
+ * @size: byte size of array element.
+ *
+ * Return: if nmemb or size is 0, then _calloc returns NULL
+ *         If malloc fails, then _calloc returns NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+	char *fill;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
+		return (NULL);
+
+	fill = mem;
+
+	for (index = 0; index < (size * nmemb); index++)
+		fill[index] = '\0';
+
+	return (mem);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,4 @@
 0x0C. C - More malloc, free
 - Task 0. Trust no one
 - Task 1. string_nconcat
+- Task 2. _calloc
