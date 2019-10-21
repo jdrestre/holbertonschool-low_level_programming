3be2cc8b20d7f6993c152974af8ea6e045c150be
jdrestre <1238@holbertonschool.com>
2019-10-21 21:41:10 +0000

    correcting the parentheses to obtain absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x < 0) ? (x * (-1)) : (x))
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif /* _funtion_like_macro_h_ */
