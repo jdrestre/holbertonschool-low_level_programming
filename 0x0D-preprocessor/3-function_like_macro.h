83876d653ce11cd69d93055eba9301728893a6d8
jdrestre <1238@holbertonschool.com>
2019-10-21 21:32:40 +0000

    funtion for macro ABS(x) that computes the absolute value of a number x

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x < 0) ? (x * (-1)) : (x))
+
+#endif /* _funtion_like_macro_h_ */
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -2,3 +2,4 @@
 - Task 0. Object-like Macro
 - Task 1. Pi
 - Task 2. File name
+- Task 3. Function-like macro
