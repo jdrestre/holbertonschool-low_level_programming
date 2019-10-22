8d8cc33852efcece6d2345e19dfb7ff04137ed3c
jdrestre <1238@holbertonschool.com>
2019-10-22 00:23:58 +0000

    Trying to understand the syntax of a structure and how to use it. A structure allows you to combine data elements of different types - Define a new type struct with name, age and owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dogs
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int _putchar(char c);
+
+#endif /* _DOG_H_ */
