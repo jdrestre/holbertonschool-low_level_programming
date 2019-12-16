98590d746c459358980c9334bb39d41533377843
jdrestre <jdrestre79@gmail.com>
2019-12-16 03:32:09 +0000

    correction fix betty, identation betty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,13 +16,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!head)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
-		     if (!new)
-			     return (NULL);
-		     new->n = n;
-		     new->next = *head;
-		     new->prev = NULL;
-		     if (*head)
-			     (*head)->prev = new;
-		     *head = new;
-		     return (new);
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
 }
