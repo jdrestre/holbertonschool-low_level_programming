89dc6f368887de4d2503de24cd25801ed88ad774
jdrestre <jdrestre79@gmail.com>
2020-01-20 04:15:30 +0000

    39 alloc 37 free, where add more free :(

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -22,10 +22,13 @@ void hash_table_delete(hash_table_t *ht)
 			while (node != NULL)
 			{
 				tmp = node->next;
+				free(node->key);
+				free(node->value);
 				free(node);
 				node = tmp;
 			}
 		}
 	}
+	free(head->array);
 	free(head);
 }
