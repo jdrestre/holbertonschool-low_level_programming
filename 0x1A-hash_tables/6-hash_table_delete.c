5ed4f61cac72b45ba65c82b58f40344cd1ec15f4
jdrestre <jdrestre79@gmail.com>
2020-01-20 05:29:56 +0000

    trying i don renunce

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,25 +10,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
-	unsigned long int i;
 
-	for (i = 0; i < ht->size; ++i)
+	hash_node_t *node, *tmp;
+	unsigned long int j;
+
+	if (ht == NULL)
+		return;
+	for (j = 0; j < ht->size; j++)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[j];
+		while (node)
 		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
-			}
+       			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
 		}
 	}
-	free(head->array);
-	free(head);
+	free(ht->array);
+	free(ht);
 }
