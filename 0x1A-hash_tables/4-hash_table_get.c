5a9bea01d6064dfd551806f6176c905d7013c010
jdrestre <jdrestre79@gmail.com>
2020-01-20 02:31:39 +0000

    updating code and restrictions about NULL if key couldnt be found

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,10 +15,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 	unsigned long int index;
 
-	/**
-	 * if (ht == NULL || key == NULL || *key == '\0')
-	 * return (NULL);
-	 */
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
