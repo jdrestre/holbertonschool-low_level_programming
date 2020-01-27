8db0b8cd4d88198b67efd158418ae76138df5c3b
jdrestre <jdrestre79@gmail.com>
2020-01-27 00:07:09 +0000

    set a hash table and try to fully a hash table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -38,4 +38,89 @@ shash_table_t *shash_table_create(unsigned long int size)
 	return (ht);
 }
 
+/**
+ * shash_table_set - adds elements to sorted hash table
+ * @ht: pointer to sorted hash table
+ * @key: ket to add - not empty string
+ * @value: key value associated
+ *
+ * Return: Failure - 0 and other 1
+ */
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+	shash_node_t *new, *tmp;
+	char *value_copy;
+	unsigned long int index;
 
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+	{
+		return (0);
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->shead;
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			/**free(tmp->value);*/
+			tmp->value = value_copy;
+			return (1);
+		}
+		tmp = tmp->snext;
+	}
+
+	new = malloc(sizeof(shash_node_t));
+	if (new == NULL)
+	{
+		/**free(value_copy);*/
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		/**free(value_copy);*/
+		/**free(new);*/
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	if (ht->shead == NULL)
+	{
+		new->sprev = NULL;
+		new->next = NULL;
+		ht->shead = new;
+		ht->stail = new;
+	}
+	else if (strcmp(ht->shead->key, key) > 0)
+	{
+		new->sprev = NULL;
+		new->snext = ht->shead;
+		ht->shead->sprev = new;
+		ht->shead = new;
+	}
+	else
+	{
+		tmp = ht->shead;
+		while (tmp->next != NULL && strcmp(tmp->snext->key, key) < 0)
+			tmp = tmp->snext;
+		new->sprev = tmp;
+		new->snext = tmp->snext;
+		if (tmp->snext == NULL)
+		{
+			ht->stail = new;
+		}
+		else
+		{
+			tmp->snext->sprev = new;
+		}
+		tmp->snext = new;
+	}
+	return (1);
+}
