017152c7c7659a957b847a8601f75d73fa6e285f
jdrestre <jdrestre79@gmail.com>
2020-01-30 19:21:14 +0000

    the output is correct, first attemp to check

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - function that returns the number of bits you would
+ need to flip to get from one number to another.
+ * @n: number of bit
+ * @m: number to get a bit
+ *
+ * Return: number of bit to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m, bits = 0;
+
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
+	return (bits);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -18,3 +18,4 @@ Task number/ Name|Task description
 - Task 2. 10| Write a function that returns the value of a bit at a given index.
 - Task 3. 11| Write a function that sets the value of a bit to 1 at a given index.
 - Task 4. 100| Write a function that sets the value of a bit to 0 at a given index.
+- Task 5. 101| Write a function that returns the number of bits you would need to flip to get from one number to another.
