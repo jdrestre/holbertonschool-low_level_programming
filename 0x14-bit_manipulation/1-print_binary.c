1a530f4c00239dd0c293a9afb523edbc4cbeaff9
jdrestre <jdrestre79@gmail.com>
2020-01-30 17:17:43 +0000

    correction code, I found a new solution

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,24 +10,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int conta, ban;
-	unsigned long int k;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	conta = (sizeof(n) * 8 - 1);
-	ban = 0;
-
-	while (conta >= 0)
-	{
-		k = n >> conta;
-
-		if (k & 1)
-		{
-			_putchar ('1');
-			ban = 1;
-		}
-
-		else if ((ban == 1) || ((conta == 0) && (ban == 0)))
-			_putchar('0');
-		conta = conta - 1;
-	}
+	_putchar((n & 1) + '0');
 }
